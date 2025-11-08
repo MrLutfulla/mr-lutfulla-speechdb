9be9ac107c79a571505736df9fd6e9eb0f5ad134
Lutfulla Murodjonov <murodjonovlutfulla@gmail.com>
2025-11-08 07:40:25 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,6 +4,7 @@ export const firebaseConfig = {
   appId: '1:349485864781:web:b693f93be623223a7e0904',
   apiKey: 'AIzaSyAR0OF3dOV6ObO12WpkP46-QFbe62Wim7Q',
   authDomain: 'mrlutfulla-speechdb-6445-47985.firebaseapp.com',
+  storageBucket: 'mrlutfulla-speechdb-6445-47985.appspot.com',
   measurementId: '',
   messagingSenderId: '349485864781',
 };
